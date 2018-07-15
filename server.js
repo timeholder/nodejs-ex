567a2d1dd98f7b8fc3f8ab5c7e67c927b3977249
Mikhail Shkurko <mishkurko@sberbank.ru>
2018-07-15 23:56:11 +0300

    test2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(morgan('combined'))
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    mongoURLLabel = mongoURL;
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
